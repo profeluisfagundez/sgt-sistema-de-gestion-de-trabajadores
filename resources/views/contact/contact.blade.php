a67ff5829046b7a157684298ad3ced1b07613ee6
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-06-18 08:00:14 -0300

    se agrega el boton de cierre de sesion

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -4,15 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contacto</title>
-    <link rel="stylesheet" href="{{ asset('css/contact.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('css/contact.css') }}"> <!-- Enlace al CSS -->
 </head>
 <body>
     <header>
         <nav>
             <ul>
-                <li><a href="{{ url('/home') }}">Inicio</a></li>
+                <li><a href="{{ url('/') }}">Inicio</a></li>
                 <li><a href="{{ url('/contact') }}">Contacto</a></li>
                 <li><a href="{{ url('/workers') }}">Trabajadores</a></li>
+                @auth
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit">Cerrar Sesión</button>
+                        </form>
+                    </li>
+                @endauth
             </ul>
         </nav>
     </header>
